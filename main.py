0c9c8eefb25fa7bca4cc4107189d1bc3af8a653b
perxpective <65282792+perxpective@users.noreply.github.com>
2020-06-02 20:52:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ def fun():
   1) Normal
   2) Slant                  (Say the Number)
   3) 5 Line Oblique
+  4) 3D
+  5) Banner 3D 
+  6) Isometric1
+  7) Bulbhead
   ''')
 
   if font == "2":
@@ -28,6 +32,18 @@ def fun():
     print(mresult)
   elif font == '3':
     newword = pyfiglet.figlet_format(word, font = '5lineoblique')
+    print(newword)
+  elif font == '4':
+    newword = pyfiglet.figlet_format(word, font = '3-d')
+    print(newword)
+  elif font == '5':
+    newword = pyfiglet.figlet_format(word, font = 'banner3-D')
+    print(newword)
+  elif font == '6':
+    newword = pyfiglet.figlet_format(word, font = 'isometric1')
+    print(newword)
+  elif font == '7':
+    newword = pyfiglet.figlet_format(word, font = 'bulbhead')
     print(newword)
   else:
       print("Not a valid number. Try again")
